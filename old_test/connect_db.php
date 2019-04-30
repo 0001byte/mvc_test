91080c50dafcf8baf4a3de8678a2693043e86993
Evgeniy Cherednichenko <0001byte@gmail.com>
2019-05-01 00:11:37 +0800

    deleted old needless folder

--- old_test/connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mysqli = new mysqli('localhost', 'root', '', 'test_login');
-
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-
-?>
--- old_test/quit.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-	session_start();
-	if(isset($_SESSION['email'])){
-	echo "Приходите ещё,".$_SESSION['email']." :(";
-		$_SESSION = array(); 
-		session_destroy();
-	}else{ echo "Чтобы завершить сеанс, нужно его начать :)";}
-?>
